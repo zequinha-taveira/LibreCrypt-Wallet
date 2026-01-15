573008a3fe4c6f136447ef37cb3b5a37640a0e8b
zequinha-taveira <zequinha.taveira.criptomoeda@gmail.com>
2026-01-15 15:42:03 -0300

    feat: Add BIP-39 English wordlist declaration header.

---
+++ firmware/include/crypto/bip39_wordlist.h
@@ -0,0 +1,16 @@
+/**
+ * @file bip39_wordlist.h
+ * @brief BIP-39 English Wordlist (2048 words)
+ *
+ * Official English wordlist from BIP-39 specification.
+ * https://github.com/bitcoin/bips/blob/master/bip-0039/english.txt
+ */
+
+#ifndef LIBRECRYPT_BIP39_WORDLIST_H
+#define LIBRECRYPT_BIP39_WORDLIST_H
+
+#define BIP39_WORDLIST_SIZE 2048
+
+extern const char *const BIP39_WORDLIST[BIP39_WORDLIST_SIZE];
+
+#endif // LIBRECRYPT_BIP39_WORDLIST_H
